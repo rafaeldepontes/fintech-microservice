dc51a5aeb7647db872c4b6f2aab25dbc4a4692c9
S1mp3r <rafaelcarneiro19@edu.unifor.br>
2025-10-24 19:38:15 -0300

    refactor(ClientApplication): changing folder structure because of an error in spring

--- client-api/src/main/java/br/rafael/client/microservice/ClientApplication.java
+++ client-api/src/main/java/br/rafael/client/microservice/ClientApplication.java
@@ -1,4 +1,4 @@
-package br.rafael.client.microservice.api;
+package br.rafael.client.microservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
