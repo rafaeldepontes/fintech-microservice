dc17de3febe9eb2d8f76b3807cb6204d028e578d
S1mp3r <rafaelcarneiro19@edu.unifor.br>
2025-10-25 12:23:31 -0300

    feat(Credit Validator Service): setting up the credit validator service

---
+++ credit-validator-api/src/main/java/br/rafael/creditvalidator/ValidateCreditApplication.java
@@ -0,0 +1,13 @@
+package br.rafael.creditvalidator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValidateCreditApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValidateCreditApplication.class, args);
+	}
+
+}
---
+++ credit-validator-api/src/test/java/br/rafael/creditvalidator/ValidateCreditApplicationTests.java
@@ -0,0 +1,13 @@
+package br.rafael.creditvalidator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValidateCreditApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
