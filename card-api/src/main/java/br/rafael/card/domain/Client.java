3545e28d7c06981129df7da81894eba89153f02e
S1mp3r <rafaelcarneiro19@edu.unifor.br>
2025-10-25 11:54:13 -0300

    chore(Card Service): removing comment

--- card-api/src/main/java/br/rafael/card/domain/Client.java
+++ card-api/src/main/java/br/rafael/card/domain/Client.java
@@ -11,7 +11,6 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -37,7 +36,6 @@ public class Client {
     private String cpf;
 
     @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "client")
-    // @JoinColumn(name = "id_card")
     private List<Card> cards;
     
     private BigDecimal incomeLimit;
