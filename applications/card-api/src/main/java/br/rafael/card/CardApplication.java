f29b8b4bdabe139b1101b1f28b119e837c645921
S1mp3r <rafaelcarneiro19@edu.unifor.br>
2025-10-26 12:56:28 -0300

    feat(CardApi): adding rabbitmq for the card consumer

--- applications/card-api/src/main/java/br/rafael/card/CardApplication.java
+++ applications/card-api/src/main/java/br/rafael/card/CardApplication.java
@@ -1,9 +1,11 @@
 package br.rafael.card;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableRabbit
 public class CardApplication {
 
 	public static void main(String[] args) {
