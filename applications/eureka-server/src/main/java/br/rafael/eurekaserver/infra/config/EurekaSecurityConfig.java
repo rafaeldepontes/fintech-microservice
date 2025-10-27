877a71f130c4ffee50d68aacb14199421dd93fb5
S1mp3r <rafaelcarneiro19@edu.unifor.br>
2025-10-27 14:13:35 -0300

    chore(Eureka Server): removing useless things

--- applications/eureka-server/src/main/java/br/rafael/eurekaserver/infra/config/EurekaSecurityConfig.java
+++ applications/eureka-server/src/main/java/br/rafael/eurekaserver/infra/config/EurekaSecurityConfig.java
@@ -1,15 +1,10 @@
 package br.rafael.eurekaserver.infra.config;
 
-import java.util.List;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
